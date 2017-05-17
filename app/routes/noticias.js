354d3d5a617a62a35d1bff222c7b8a0a1f270100
Filipozo <Filipozo>
2017-05-17 12:15:36 -0300

    seção 5, aula 24

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,11 @@
+const dbConnection = require('../../config/dbConnection');
+
 module.exports = function (app) {
+    const connection = dbConnection();
     app.get('/noticias', (req, res) => {
-        res.render('noticias/noticias');
+        connection.query('select * from noticias', (error, result) => {
+           // res.send(result);
+           res.render('noticias/noticias', { noticia: result });
+        });
     });
 };
